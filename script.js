b69d8bc783d30221e31a2b75455543767f4df95b
Hana <hanaadhami01@gmail.com>
2019-10-12 23:11:13 -0700

    created questions in JS

--- script.js
+++ script.js
@@ -22,4 +22,62 @@ setTime();
 timeEl.setAttribute("style", "margin:auto; width:50%; text-align:right;");
 mainEl.setAttribute("style", "margin:auto; width:50%; text-align:right;");
 
-//start button
+//Create all the elements into variables
+
+var quizContainer = document.querySelector(".container")
+var startBtn = document.querySelector(".startbutton")
+var submitBtn = document.querySelector(".submitbutton")
+var answerBtn = document.querySelector(".answerbutton")
+var resultsDiv = document.querySelector(".results")
+
+// Create functions
+
+function buildquiz(){
+
+
+}
+
+function showResults(){
+
+
+}
+
+buildquiz();
+
+submitBtn.addEventListener('click', showResults);
+
+// displaying quiz questions
+
+var myQuestions = [
+{
+  question: "What is an object literal?"
+
+  answers: {
+    a: "A list of zero or more pairs of property names and associated values of an object, enclosed in curly braces ({}).",
+    b: "A pattern enclosed between slashes.",
+    c: "Zero or more characters enclosed in double or single quotation marks."
+  },
+  correctAnswer: "a"
+},
+{
+  question: "Which of the following is not a data type?"
+
+  answers: {
+    a: "Boolean",
+    b: "String",
+    c: "Element"
+  },
+  correctAnswer: "c"
+},
+{
+  question: "Which of the following converts strings to numbers?"
+  
+  answers: {
+    a: "parseInt",
+    b: "convertNum",
+    c: "string2num"
+  },
+  correctAnswer: "a"
+}
+
+]
